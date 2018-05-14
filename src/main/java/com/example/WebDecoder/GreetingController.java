04d023da00385db15cd5fb75d74ec4680d7d5404
Mike Savenkov <mv.savenkov@stalcom.com>
2018-05-14 18:02:49 +0300

    add replaceSymbol2

--- src/main/java/com/example/WebDecoder/GreetingController.java
+++ src/main/java/com/example/WebDecoder/GreetingController.java
@@ -3,6 +3,7 @@ package com.example.WebDecoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,8 +16,12 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.gson.Gson;
+
 @Controller
 public class GreetingController {
+
+		Gson GSON = new Gson();
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false) String name, Model model) throws IOException {
@@ -147,9 +152,88 @@ public class GreetingController {
                 }
             }
         }
+        String json = GSON.toJson(sortMap);
 
         String newDecodedText = new String(charArray);
+        model.addAttribute("oldText", decodedText);
         model.addAttribute("text", newDecodedText);
+        model.addAttribute("sourceSymbol", sourceSymbol);
+        model.addAttribute("targetSymbol", targetSymbol);
+        model.addAttribute("json", json);
         return "replaceSymbol";
     }
+
+	@PostMapping("/decoding/replaceSymbol2")
+	String replaceSymbol2(@RequestParam("sourceSymbol") char sourceSymbol,
+	                     @RequestParam("targetSymbol") char targetSymbol,
+	                     @RequestParam("decodedText") String decodedText,
+	                     @RequestParam("json") String json,
+	                     Model model) throws IOException {
+
+		Map<Character, Integer> sortMapEncoded = GSON.fromJson(json, Map.class);
+
+
+		StatisticTextEncoded statisticTextEncoded = new StatisticTextEncoded();
+		StatisticTextsIT statisticTextsIT = new StatisticTextsIT();
+		Frequency frequency = new Frequency();
+
+		//	      char[] charArray = decodedText.toCharArray();
+		//	      for (int i = 0; i < charArray.length; i++) {
+		//		        if (charArray[i] == sourceSymbol) {
+		//		        	  charArray[i] = targetSymbol;
+		//		        }
+		//	      }
+		String encodedText = statisticTextEncoded.encodedText();
+		//Map<Character, Integer> sortMapEncoded = statisticTextEncoded.encodedMap();
+
+
+		Integer freqSourceSymbol = frequency.frequency(String.valueOf(sourceSymbol), decodedText);
+		Integer freqTargetSymbol = frequency.frequency(String.valueOf(targetSymbol), decodedText);
+
+
+		Character symbol1 = ' ';
+		Character symbol2 = ' ';
+
+		//ищем соответствие букв по частоте в зашифрованном тексте
+		for (Map.Entry entry : sortMapEncoded.entrySet()) {
+			if (freqSourceSymbol == entry.getValue()) {
+				symbol1 = (char) entry.getKey();// 'Л'
+			}
+			if (freqTargetSymbol == (int) entry.getValue()) {
+				symbol2 = (char) entry.getKey();  // 'У'
+			}
+		}
+
+		//меняем value местами
+		sortMapEncoded.replace(symbol1, freqTargetSymbol);
+		sortMapEncoded.replace(symbol2, freqSourceSymbol);
+
+		//сортируем
+		final Map<Character, Integer> sortMap = new LinkedHashMap<>();
+		sortMapEncoded.entrySet().stream()
+		              .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+		              .forEachOrdered(x -> sortMap.put(x.getKey(), x.getValue()));
+
+
+		List<Character> chars = statisticTextsIT.chars();
+		List<Character> charsEncoded = new ArrayList<>(sortMap.keySet());
+
+
+		char[] charArray = encodedText.toCharArray();
+		for (int i = 0; i < charArray.length; i++) {
+			for (int j = 0; j < chars.size(); j++) {
+				if (charArray[i] == charsEncoded.get(j)) {
+					charArray[i] = chars.get(j);
+					break;
+				}
+			}
+		}
+
+		String newDecodedText = new String(charArray);
+		model.addAttribute("oldText", decodedText);
+		model.addAttribute("text", newDecodedText);
+		model.addAttribute("sourceSymbol", sourceSymbol);
+		model.addAttribute("targetSymbol", targetSymbol);
+		return "replaceSymbol2";
+	}
 }
